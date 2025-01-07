ae870d5c907e58857099c3f0538ddfd68fb4dfb9
Kestin Goforth <kgoforth1503@gmail.com>
2025-01-07 08:17:48 -0500

    Create dump_tables.py

---
+++ dump_tables.py
@@ -0,0 +1,31 @@
+import csv
+import os
+
+import pyodbc
+
+SERVER = "(local)"
+DATABASE = "carcom"
+USERNAME = "Asuser"
+PASSWORD = "GunnarS3g3"
+
+connectionString = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}"
+conn = pyodbc.connect(connectionString)
+cursor = conn.cursor()
+
+cursor.execute("SELECT DISTINCT [TABLE_NAME] FROM INFORMATION_SCHEMA.TABLES")
+for _table in cursor.fetchall():
+
+    table = _table.TABLE_NAME
+    cursor.execute(f"SELECT * FROM [{table}]")
+    rows = cursor.fetchall()
+
+    outpath = f"tables/{DATABASE}/{table}.csv"
+    os.makedirs(os.path.split(outpath)[0], exist_ok=True)
+    with open(outpath, "w+", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([x[0] for x in cursor.description])  # column headers
+        for row in rows:
+            writer.writerow(row)
+
+cursor.close()
+conn.close()
