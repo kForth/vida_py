c795ba88ae94ac3c3cd4d687ef5e0aaa9a33e317
Kestin Goforth <kgoforth1503@gmail.com>
2025-01-07 14:08:06 -0500

    Update get_profile.py

--- get_profile.py
+++ get_profile.py
@@ -1,13 +1,25 @@
+import pyodbc
+
 from model_info import ENGINES_BY_NAME, MODELS_BY_NAME
+from sql_funcs import get_profile
 
-year = 2004
-model = "V70 XC (01-) / XC70 (-07)"
-# model = "V70 (00-08)"
-engine = "B5254T2"
+SERVER = "(local)"
+DATABASE = "carcom"
+USERNAME = "Asuser"
+PASSWORD = "GunnarS3g3"
 
-_model = MODELS_BY_NAME[model]
-_engine = ENGINES_BY_NAME[engine]
+connectionString = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}"
+conn = pyodbc.connect(connectionString)
+cursor = conn.cursor()
 
-profile = f"mdl{_model}yr{year}eng{_engine}"
+profiles = get_profile(
+    cursor,
+    year=2650,  # 2004
+    market=2743,  # AME
+    steering=2750,  # LHD
+    transmission=2640,  # AW50/51 AWD
+    engine=2732,  # B5254T2
+    model=2687,  # "V70 XC (01-) / XC70 (-07)"
+)
 
-print(profile)
+print(profiles)
