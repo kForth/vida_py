9d6a4ecb0e5fae2878e50dfd505de09542cec303
Kestin Goforth <kgoforth1503@gmail.com>
2025-01-08 18:09:54 -0500

    Update decode_vin.py

--- vida_py/cli/decode_vin.py
+++ vida_py/cli/decode_vin.py
@@ -10,13 +10,15 @@ from vida_py.scripts.diag import get_vin_components
 def main(vin):
     session = sessionmaker(bind=diag)()
 
-    profile = get_vin_components(session, vin)[0]
+    (model_id, model_str, model_year, engine_id, engine_str, transm_id, transm_str) = (
+        get_vin_components(session, vin)[0]
+    )
 
     click.echo(f"VIN: {vin}")
-    click.echo(f"Model: {profile[1]} [{profile[0]}]")
-    click.echo(f"Year: {profile[2]}")
-    click.echo(f"Engine: {profile[4]} [{profile[3]}]")
-    click.echo(f"Transmission: {profile[6]} [{profile[5]}]")
+    click.echo(f"Model: {model_str} [{model_id}]")
+    click.echo(f"Year: {model_year}")
+    click.echo(f"Engine: {engine_str} [{engine_id}]")
+    click.echo(f"Transmission: {transm_str} [{transm_id}]")
     click.echo(f"Chassis: {vin[-6:]}")
 
     session.close()
