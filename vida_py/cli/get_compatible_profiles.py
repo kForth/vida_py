7f2dfd0a7c0e9ed2d566f71345e6c1c04d69e943
Kestin Goforth <kgoforth1503@gmail.com>
2025-01-08 21:53:07 -0500

    Create get_compatible_profiles.py

---
+++ vida_py/cli/get_compatible_profiles.py
@@ -0,0 +1,31 @@
+import click
+from models.basedata import VehicleProfile
+from sqlalchemy.orm import sessionmaker
+
+from vida_py.db import basedata
+from vida_py.funcs import run_func
+
+
+@click.command()
+@click.argument("profile_id", type=click.STRING)
+def main(profile_id):
+    basedata_session = sessionmaker(bind=basedata)()
+
+    profiles_ids = [
+        e[0]
+        for e in run_func(basedata_session, "GetValidProfileManager", profile_id).all()
+    ]
+    profiles = (
+        basedata_session.query(VehicleProfile)
+        .where(VehicleProfile.Id.in_(profiles_ids))
+        .all()
+    )
+
+    for profile in profiles:
+        print(f"{profile.Id}: {profile.Description}")
+
+    basedata_session.close()
+
+
+if __name__ == "__main__":
+    main()
