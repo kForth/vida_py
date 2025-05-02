91b2a2d77c8eb381c55f95a08b1caf58f3b85a22
Kestin Goforth <kgoforth1503@gmail.com>
2025-05-02 14:00:46 -0400

    Update get_ecu_log_params.py

--- examples/get_ecu_log_params.py
+++ examples/get_ecu_log_params.py
@@ -1,24 +1,14 @@
 import json
 
 import click
+from sqlalchemy.orm import aliased
 
 from vida_py.carcom import (
     Session,
     T100_EcuVariant,
-    T110_Service_EcuVariant,
-    T111_Service,
-    T120_Config_EcuVariant,
-    T121_Config,
     T141_Block,
-    T143_BlockDataType,
     T144_BlockChild,
-    T148_BlockMetaPARA,
     T150_BlockValue,
-    T170_SecurityCode_EcuVariant,
-    T171_SecurityCode,
-    T191_TextData,
-    T193_Language,
-    T155_Scaling,
 )
 
 @click.command()
@@ -27,54 +17,64 @@ from vida_py.carcom import (
 @click.option("--outfile", "-o", type=click.Path(dir_okay=False))
 def main(identifier, language, outfile):
     with Session() as session:
-        """
-        SELECT tbv.fkT155_Scaling, tbv.CompareValue, b.*FROM carcom.dbo.T141_Block b
-        join carcom.dbo.T100_EcuVariant tev
-        on tev.identifier = '08658507  H'
-        join carcom.dbo.T150_BlockValue tbv
-        on tbv.fkT141_Block = b.id
-        WHERE
-        b.name LIKE '%PLSOL%'
-        AND
-        b.fkT142_BlockType = 5
-        """
+
+        T141_Block_2 = aliased(T141_Block)
+        T150_BlockValue_2 = aliased(T150_BlockValue)
+        T144_BlockChild_2 = aliased(T144_BlockChild)
+
         params = (
-            session.query(T141_Block, T150_BlockValue, T155_Scaling)
-            .join(T150_BlockValue, T150_BlockValue.fkT141_Block == T141_Block.id)
-            .join(T144_BlockChild, T144_BlockChild.fkT141_Block_Child == T141_Block.id)
-            .join(T155_Scaling, T155_Scaling.id == T150_BlockValue.fkT155_Scaling)
-            .join(
+            session.query(
+                T141_Block, T150_BlockValue, T141_Block_2, T150_BlockValue_2
+            ).join(
+                T144_BlockChild, T144_BlockChild.fkT141_Block_Child == T141_Block.id
+            ).join(
+                T150_BlockValue, T150_BlockValue.fkT141_Block == T141_Block.id
+            ).join(
+                T144_BlockChild_2, T144_BlockChild_2.fkT141_Block_Parent == T141_Block.id
+            ).join(
+                T141_Block_2, T141_Block_2.id == T144_BlockChild_2.fkT141_Block_Child
+            ).join(
+                T150_BlockValue_2, T150_BlockValue_2.fkT141_Block == T141_Block_2.id
+            ).join(
                 T100_EcuVariant, T100_EcuVariant.id == T144_BlockChild.fkT100_EcuVariant
-            )
-            .filter(
-                T141_Block.fkT142_BlockType == 5,
+            ).where(
                 T100_EcuVariant.identifier == identifier,
+                T144_BlockChild.fkT141_Block_Parent == 1,
+                T141_Block.fkT142_BlockType == 5
+            ).order_by(
+                T144_BlockChild.fkT141_Block_Parent,
+                T144_BlockChild.fkT141_Block_Child
             )
-            .order_by(
-                T141_Block.id,
-            )
+
             .all()
         )
 
         _params = []
-        for block, value, scaling in params:
+        for block, value, block2, value2 in params:
             _params.append({
+                "id": block.id,
+                "type": block.type.identifier,
+                "datatype": block.datatype.name,
                 "name": block.name,
                 "offset": block.offset,
                 "length": block.length,
-                "scaling": scaling.definition,
-                "scaling_id": scaling.id,
-                "scaling_type": scaling.type,
-                "text_value": "\n".join(e.data for e in value.text_value.data),
-                "text_unit": "\n".join(e.data for e in value.text_unit.data),
-                "compare": value.CompareValue,
+                "exclude": block.exclude,
+                "composite": block.composite,
+                "value_id": value.Id,
+                "sort_order": value.SortOrder,
+                "identifier": value.CompareValue,
+                "operator": value.Operator,
+                "name2": block2.name,
+                "text": "".join(e.data for e in block2.text.data if e.language.identifier == language),
+                "scaling": value2.ppe_scaling.definition,
+                "unit": "".join(e.data for e in value2.ppe_text_unit.data if e.language.identifier == language),
             })
 
         if outfile:
             with open(outfile, "w+", encoding="utf-8") as out:
-                json.dump(_params, out, indent=4)
+                json.dump(_params, out, indent=2)
         else:
-            click.echo(json.dumps(_params, indent=4))
+            click.echo(json.dumps(_params, indent=2))
 
 
 if __name__ == "__main__":
