2bdfecf5516430151971815c564b43be22f17f22
Kestin Goforth <kgoforth1503@gmail.com>
2025-05-29 12:41:21 -0400

    Fix basedata scripts

--- vida_py/basedata/scripts.py
+++ vida_py/basedata/scripts.py
@@ -10,8 +10,12 @@ def clean_up(session: Session, dest_database: str) -> List[Row]:
     return run_script(session, "CleanUp", DestDatabase=dest_database).all()
 
 
-def get_engines_for_model_and_model_year(session: Session, model: int) -> List[Row]:
-    return run_script(session, "getEnginesForModelAndModelYear", model=model).all()
+def get_engines_for_model_and_model_year(
+    session: Session, model: int, year: int
+) -> List[Row]:
+    return run_script(
+        session, "getEnginesForModelAndModelYear", model=model, year=year
+    ).all()
 
 
 def get_valid_profiles_for_selected_builder(
@@ -22,17 +26,35 @@ def get_valid_profiles_for_selected_builder(
     ).all()
 
 
-def get_vin_components(session: Session, vin: str) -> List[Row]:
-    return run_script(session, "getVINComponents", vin=vin).all()
+def get_vin_components(session: Session, vin: str, partner_group_cid: str) -> List[Row]:
+    return run_script(
+        session, "getVINComponents", vin=vin, partnerGroupCid=partner_group_cid
+    ).all()
 
 
-def get_vin_components_by_partner_group_id(session: Session, vin: str) -> List[Row]:
-    return run_script(session, "getVINComponentsByPartnerGroupId", vin=vin).all()
+def get_vin_components_by_partner_group_id(
+    session: Session, vin: str, partner_group_id: int
+) -> List[Row]:
+    return run_script(
+        session,
+        "getVINComponentsByPartnerGroupId",
+        vin=vin,
+        partnerGroupId=partner_group_id,
+    ).all()
 
 
-def get_vin_components_by_partner_group_id_swdl(session: Session, vin: str) -> List[Row]:
-    return run_script(session, "getVINComponentsByPartnerGroupIdSwdl", vin=vin).all()
+def get_vin_components_by_partner_group_id_swdl(
+    session: Session, vin: str, partner_group_id: int
+) -> List[Row]:
+    return run_script(
+        session,
+        "getVINComponentsByPartnerGroupIdSwdl",
+        vin=vin,
+        partnerGroupId=partner_group_id,
+    ).all()
 
 
-def sp__return_csv(session: Session, column_name: str) -> List[Row]:
-    return run_script(session, "sp_ReturnCSV", ColumnName=column_name).all()
+def sp__return_csv(session: Session, column_name: str, table_name: str) -> List[Row]:
+    return run_script(
+        session, "sp_ReturnCSV", ColumnName=column_name, TableName=table_name
+    ).all()
