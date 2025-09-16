dd357a3028850172d0de35fcc4050a02c9d1edae
Kestin Goforth <kgoforth1503@gmail.com>
2025-09-16 13:20:45 -0400

    Update bytes_fo_file.py

--- examples/bytes_fo_file.py
+++ examples/bytes_fo_file.py
@@ -1,20 +1,28 @@
 import click
 
+
 @click.command()
-@click.argument("bytestr", type=click.STRING)
+@click.option("--bytestr", "-s", type=click.STRING)
+@click.option("--bytefile", "-f", type=click.Path(dir_okay=False, exists=True))
 @click.option("--outfile", "-o", type=click.Path(dir_okay=False), default="output.bin")
-def main(bytestr, outfile):
+def main(bytestr, bytefile, outfile):
     """
     Convert a string of bytes to a file.
     """
+
+    if bytefile:
+        with open(bytefile, encoding="utf-8") as src:
+            bytestr = src.read()
+
     # Convert the string to bytes
-    byte_array = bytes.fromhex(bytestr[2 if bytestr.startswith("0x") else 0:])
+    byte_array = bytes.fromhex(bytestr[2 if bytestr.startswith("0x") else 0 :])
 
     # Write the bytes to a file
     with open(outfile, "wb") as f:
         f.write(byte_array)
 
     click.echo(f"Bytes written to {outfile}")
+
 
 if __name__ == "__main__":
     main()
