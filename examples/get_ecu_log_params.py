df40c0fb617b0044f5d52e8dc1ab2ff718c6f1d4
Kestin Goforth <kgoforth1503@gmail.com>
2025-09-23 15:56:23 -0400

    Update get_ecu_log_params.py

--- examples/get_ecu_log_params.py
+++ examples/get_ecu_log_params.py
@@ -8,7 +8,7 @@ from vida_py.carcom import (
     T100_EcuVariant,
     T141_Block,
     T144_BlockChild,
-    T150_BlockValue,
+    T148_BlockMetaPARA,
 )
 
 LANGUAGES = (
@@ -44,84 +44,86 @@ LANGUAGES = (
 def main(identifier, language, outfile):
     with Session() as session:
 
-        T141_Block_2 = aliased(T141_Block)
-        T150_BlockValue_2 = aliased(T150_BlockValue)
-        T144_BlockChild_2 = aliased(T144_BlockChild)
+        T141_Block_Parent = aliased(T141_Block)
 
-        # string cmdText = $"select
-        # val=case fkt155_scaling
-        #   when 0 then fkt190_text_ppevalue
-        #   else fkt190_text_value
-        #   end,
-        # unit=case fkt155_scaling
-        #   when 0 then fkt190_text_ppeunit
-        #   else fkt190_text_unit
-        #   end
-        # from t150_blockvalue
-        #  join t141_block t141
-        #  on fkt141_block = t141.id
-        #  where t141.fkt190_text in ({parameterIds}) and t141.fkt142_blocktype=8";
-
-        # SELECT *
-        # FROM [T150_BlockValue] AS Val
-        # JOIN [T141_Block] AS Blk ON Blk.id = Val.fkT141_Block
-        # JOIN [T144_BlockChild] Chld ON Chld.fkT141_Block_Parent = Blk.Id
-        # JOIN [T148_BlockMetaPARA] AS Meta ON Meta.fkT141_Block = Chld.fkT141_Block_Child
-        # JOIN [T141_Block] AS Blk2 ON Blk2.id = Chld.fkT141_Block_Child
-        # JOIN [T143_BlockDataType] Typ ON Typ.id = Blk2.fkT143_BlockDataType
-        # WHERE Meta.fkT100_EcuVariant = 348
-
-        params = (
-            session.query(T141_Block, T150_BlockValue, T141_Block_2, T150_BlockValue_2)
+        params: list[tuple[T141_Block, T148_BlockMetaPARA, T141_Block]] = (
+            session.query(T141_Block, T148_BlockMetaPARA, T141_Block_Parent)
+            .join(T148_BlockMetaPARA, T148_BlockMetaPARA.fkT141_Block == T141_Block.id)
+            .join(
+                T100_EcuVariant,
+                T100_EcuVariant.id == T148_BlockMetaPARA.fkT100_EcuVariant,
+            )
             .join(T144_BlockChild, T144_BlockChild.fkT141_Block_Child == T141_Block.id)
-            .join(T150_BlockValue, T150_BlockValue.fkT141_Block == T141_Block.id)
             .join(
-                T144_BlockChild_2, T144_BlockChild_2.fkT141_Block_Parent == T141_Block.id
+                T141_Block_Parent,
+                T141_Block_Parent.id == T144_BlockChild.fkT141_Block_Parent,
             )
-            .join(T141_Block_2, T141_Block_2.id == T144_BlockChild_2.fkT141_Block_Child)
-            .join(T150_BlockValue_2, T150_BlockValue_2.fkT141_Block == T141_Block_2.id)
-            .join(
-                T100_EcuVariant, T100_EcuVariant.id == T144_BlockChild.fkT100_EcuVariant
-            )
-            .where(
-                T100_EcuVariant.identifier == identifier,
-                T144_BlockChild.fkT141_Block_Parent == 1,
-                T141_Block.fkT142_BlockType == 5,
-            )
-            .order_by(
-                T144_BlockChild.fkT141_Block_Parent, T144_BlockChild.fkT141_Block_Child
-            )
+            .filter(T100_EcuVariant.identifier == identifier)
             .all()
         )
 
         _params = []
-        for block, value, block2, value2 in params:
+        for block, meta, parent in params:
             _params.append(
                 {
                     "id": block.id,
-                    "type": block.type.identifier,
-                    "datatype": block2.datatype.name,
-                    "name": block.name,
+                    "name": (
+                        block.name
+                        if block.fkT190_Text == 0
+                        else [
+                            e.data
+                            for e in block.text.data
+                            if e.language.identifier == language
+                        ][0]
+                    ),
+                    "category": (
+                        parent.name
+                        if parent.fkT190_Text == 0
+                        else [
+                            e.data
+                            for e in parent.text.data
+                            if e.language.identifier == language
+                        ][0]
+                    ),
+                    "identifier": parent.values[0].CompareValue,
                     "offset": block.offset,
                     "length": block.length,
-                    "exclude": block.exclude,
-                    "composite": block.composite,
-                    "value_id": value.Id,
-                    "sort_order": value.SortOrder,
-                    "identifier": value.CompareValue,
-                    "operator": value.Operator,
-                    "name2": block2.name,
-                    "text": "".join(
-                        e.data
-                        for e in block2.text.data
-                        if e.language.identifier == language
-                    ),
-                    "scaling": value2.ppe_scaling.definition,
-                    "unit": "".join(
-                        e.data
-                        for e in value2.ppe_text_unit.data
-                        if e.language.identifier == language
-                    ),
+                    "type": block.type.identifier,
+                    "min": float(meta.asMinRange or 0),
+                    "max": float(meta.asMaxRange or 0),
+                    "freezeframe": meta.showAsFreezeFrame,
+                    "datatype": block.datatype.name,
+                    "values": [
+                        {
+                            "id": val.Id,
+                            "compare": val.CompareValue,
+                            "operator": val.Operator,
+                            "scaling": (
+                                val.ppe_scaling
+                                if val.fkT155_Scaling == 0
+                                else val.scaling
+                            ).definition,
+                            "text": [
+                                e.data
+                                for e in (
+                                    val.ppe_text_value.data
+                                    if val.fkT155_Scaling == 0
+                                    else val.text_value.data
+                                )
+                                if e.language.identifier == language
+                            ][0],
+                            "unit": [
+                                e.data
+                                for e in (
+                                    val.ppe_text_unit.data
+                                    if val.fkT155_Scaling == 0
+                                    else val.text_unit.data
+                                )
+                                if e.language.identifier == language
+                            ][0],
+                        }
+                        for val in block.values
+                    ],
                 }
             )
 
